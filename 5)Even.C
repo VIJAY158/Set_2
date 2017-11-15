a60a51c51f90db06e2f8961ac91779a2b72410ac
VIJAY158 <30854995+VIJAY158@users.noreply.github.com>
2017-11-15 22:17:03 +0530

    Create 5)Even.C

---
+++ 5)Even.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int n1,n2,i;
+printf("Enter the limits:\n");
+scanf("%d%d",&n1,&n2);
+for(i=n1;i<=n2;i++)
+{
+    if(i%2==0)
+    {
+        printf("%d\t",i);
+    }
+}
+return 0;
+}
